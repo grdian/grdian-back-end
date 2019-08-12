ec6e2285ef9bb7e7f703740acb05e34a8288b1f0
charles-m-doan <cmatthewdoan@gmail.com>
2019-08-12 14:06:27 -0400

    Modifying alerts to include urgency and location.

--- src/main/java/grdian/grdianbackend/Initializer.java
+++ src/main/java/grdian/grdianbackend/Initializer.java
@@ -29,37 +29,38 @@ public class Initializer implements CommandLineRunner {
 	private boolean errorsDuringInitialization = false;
 
 	@Override
-	public void run(String... args) throws Exception
-		{
+	public void run(String... args) throws Exception {
 		logStartOfInitializer();
 
-		try
-			{
+		try {
 			populateRepositories();
-			}
-		catch (Exception e)
-			{
+		} catch (Exception e) {
 			errorsDuringInitialization = true;
 			logErrorAndException(e);
-			}
-		logCompletionOfInitializer();
 		}
+		logCompletionOfInitializer();
+	}
 
-	private void populateRepositories()
-		{
+	private void populateRepositories() {
 		System.out.println("POPULATE");
 
-		Grdian erlich = new Grdian("Erlich", "Bachman", "images/portraits/portrait_erlich_bachman.jpg", "0694201337", "bachmanity@piedpiper.net", "password");
+		Grdian erlich = new Grdian("Erlich", "Bachman", "images/portraits/portrait_erlich_bachman.jpg", "0694201337",
+				"bachmanity@piedpiper.net", "password");
 		erlich = grdianRepo.save(erlich);
-		Grdian bryce = new Grdian("Bryce", "Sampson", "images/portraits/portrait_bryce_sampson.jpg", "0987654321", "sampson@gmail.com", "password");
+		Grdian bryce = new Grdian("Bryce", "Sampson", "images/portraits/portrait_bryce_sampson.jpg", "0987654321",
+				"sampson@gmail.com", "password");
 		bryce = grdianRepo.save(bryce);
-		Grdian charles = new Grdian("Charles", "Doan", "images/portraits/portrait_charles_doan.jpg", "0987654321", "doan@gmail.com", "password");
+		Grdian charles = new Grdian("Charles", "Doan", "images/portraits/portrait_charles_doan.jpg", "0987654321",
+				"doan@gmail.com", "password");
 		charles = grdianRepo.save(charles);
-		Grdian lawrence = new Grdian("Lawrence", "Mboya", "images/portraits/portrait_lawrence_mboya.jpg", "1234567890", "mboya@gmail.com", "password");
+		Grdian lawrence = new Grdian("Lawrence", "Mboya", "images/portraits/portrait_lawrence_mboya.jpg", "1234567890",
+				"mboya@gmail.com", "password");
 		lawrence = grdianRepo.save(lawrence);
-		Grdian nazik = new Grdian("Nazik", "Elhag", "images/portraits/portrait_nazik_elhag.jpg", "0987654321", "elhag@gmail.com", "password");
+		Grdian nazik = new Grdian("Nazik", "Elhag", "images/portraits/portrait_nazik_elhag.jpg", "0987654321",
+				"elhag@gmail.com", "password");
 		nazik = grdianRepo.save(nazik);
-		Grdian tyler = new Grdian("Tyler", "Conner", "images/portraits/portrait_tyler_conner.jpg", "0987654321", "conner@gmail.com", "password");
+		Grdian tyler = new Grdian("Tyler", "Conner", "images/portraits/portrait_tyler_conner.jpg", "0987654321",
+				"conner@gmail.com", "password");
 		tyler = grdianRepo.save(tyler);
 
 		erlich.addGrdianToThisUser(charles);
@@ -84,13 +85,13 @@ public class Initializer implements CommandLineRunner {
 		nazik.addGrdianToThisUser(tyler);
 		nazik = grdianRepo.save(nazik);
 
-		Alert erlichAlert = new Alert(erlich, "Jian Yaaaaanngg!!");
+		Alert erlichAlert = new Alert(erlich, "Jian Yaaaaanngg!!", "Moderate", "(12.0,15.123513)");
 		alertRepo.save(erlichAlert);
 
-		Alert charlesAlert = new Alert(charles, "SQUIRRELS! SQUIRRELS EVERYWHERE!");
+		Alert charlesAlert = new Alert(charles, "SQUIRRELS! SQUIRRELS EVERYWHERE!", "EMERGENCY", "(12.231,20.89513)");
 		alertRepo.save(charlesAlert);
 
-		Alert tylerAlert = new Alert(tyler, "I'm out of hummus.");
+		Alert tylerAlert = new Alert(tyler, "I'm out of hummus.", "Minor", "(86.9841,89.1123)");
 		alertRepo.save(tylerAlert);
 
 //		Grdian grdian01 = EntityGenerator.generateRandomGrdian();
@@ -119,41 +120,34 @@ public class Initializer implements CommandLineRunner {
 //
 //		Alert alert02 = new Alert(grdian02);
 //		alert02 = alertRepo.save(alert02);
-		}
+	}
 
-	private void logStartOfInitializer()
-		{
+	private void logStartOfInitializer() {
 		logger.info("Starting Initializer");
 		startTimestamp = Instant.now();
-		}
+	}
 
-	private void logErrorAndException(Exception ex)
-		{
+	private void logErrorAndException(Exception ex) {
 		logger.error("There was a problem in the execution of the Initializer.", ex);
-		}
+	}
 
-	private void logCompletionOfInitializer()
-		{
+	private void logCompletionOfInitializer() {
 		String errors = checkForErrors();
 		logger.info("Initializer done " + errors + ", it took " + timeToCompleteInitializer() + " ms to finish.");
-		if (errors != "")
-			{
+		if (errors != "") {
 			logger.info("Terminating application...");
 			System.exit(-1);
-			}
 		}
+	}
 
-	private String checkForErrors()
-		{
-		if (errorsDuringInitialization)
-			{
+	private String checkForErrors() {
+		if (errorsDuringInitialization) {
 			return "WITH ERRORS";
-			}
+		}
 		return "";
-		}
+	}
 
-	private int timeToCompleteInitializer()
-		{
+	private int timeToCompleteInitializer() {
 		return Instant.now().compareTo(startTimestamp) / 1000000;
-		}
+	}
 }
--- src/main/java/grdian/grdianbackend/controllers/AlertController.java
+++ src/main/java/grdian/grdianbackend/controllers/AlertController.java
@@ -35,66 +35,60 @@ public class AlertController {
 	@Autowired
 	private AlertRepo alertRepo;
 
-	public AlertController()
-		{
-		}
+	public AlertController() {
+	}
 
 	@GetMapping("/allalerts")
-	public Iterable<Alert> sendAllAlerts()
-		{
+	public Iterable<Alert> sendAllAlerts() {
 		return alertRepo.findAll();
-		}
+	}
 
 	@GetMapping("/allalerts/{id}")
-	public Alert findSingleAlert(@PathVariable Long id)
-		{
+	public Alert findSingleAlert(@PathVariable Long id) {
 		return alertRepo.findById(id).get();
-		}
+	}
 
 	@GetMapping("/activealerts/{id}")
-	public Iterable<Alert> findActiveAlertsForGrdian(@PathVariable Long id)
-		{
+	public Iterable<Alert> findActiveAlertsForGrdian(@PathVariable Long id) {
 		Set<Alert> activeAlerts = new HashSet<Alert>();
 
 		Grdian grdian = grdianRepo.findById(id).get();
 		Set<Grdian> grdedUsers = grdian.getGrdedUsers();
-		for (Grdian user : grdedUsers)
-			{
+		for (Grdian user : grdedUsers) {
 			Alert activeAlert = user.getActiveAlert();
-			if (activeAlert != null)
-				{
+			if (activeAlert != null) {
 				activeAlerts.add(activeAlert);
-				}
 			}
-		return activeAlerts;
 		}
+		return activeAlerts;
+	}
 
 	@PatchMapping("/allalerts/resolve")
-	public void resolveAlert(@RequestBody String body, HttpServletResponse response) throws JSONException, IOException
-		{
+	public void resolveAlert(@RequestBody String body, HttpServletResponse response) throws JSONException, IOException {
 		JSONObject json = (JSONObject) JSONParser.parseJSON(body);
 		Long id = json.getLong("id");
 		Alert activeAlert = alertRepo.findById(id).get();
 		activeAlert.markResolved();
 		alertRepo.save(activeAlert);
 		response.sendRedirect("/api/allalerts");
-		}
+	}
 
 	@PostMapping("/allalerts")
-	public void createNewAlert(@RequestBody String jsonBody, HttpServletResponse response) throws JSONException, IOException
-		{
+	public Alert createNewAlert(@RequestBody String jsonBody, HttpServletResponse response)
+			throws JSONException, IOException {
 		JSONObject json = (JSONObject) JSONParser.parseJSON(jsonBody);
+		System.out.println(json);
 		Long senderId = json.getLong("senderId");
 		String message = json.getString("message");
+		String urgency = json.getString("urgency");
+		String location = json.getString("location");
 		Grdian sender = grdianRepo.findById(senderId).get();
 		Alert activeAlert = sender.getActiveAlert();
-		if (activeAlert != null)
-			{
+		if (activeAlert != null) {
 			activeAlert.markResolved();
-			}
-		Alert newAlert = new Alert(sender, message);
-		alertRepo.save(newAlert);
-		response.sendRedirect("/api/allalerts");
 		}
+		Alert newAlert = new Alert(sender, message, urgency, location);
+		return alertRepo.save(newAlert);
+	}
 
 }
